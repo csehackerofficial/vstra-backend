393e4f8ffb9ea0d90678ba00d09e64923a131e5b
Aayush Kumar <csehackerofficial@gmail.com>
2026-04-12 13:15:38 +0530

    Fix: Changed database to ecommerce_db

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: 'mysql-2459a884-vstra.b.aivencloud.com', // 👈 Aiven का होस्ट
+    host: 'mysql-2459a884-vstra.b.aivencloud.com', // Aiven का होस्ट
     port: 12507,
     user: 'avnadmin',
-    password: process.env.DB_PASSWORD, // 👈 इसे ऐसे ही रहने दें, असली पासवर्ड Render पर डालेंगे
-    database: 'defaultdb',
+    password: process.env.DB_PASSWORD, // पासवर्ड Render के Environment से आएगा
+    database: 'ecommerce_db', // 👈 बस यही एक बदलाव हुआ है (defaultdb से ecommerce_db)
     ssl: {
         rejectUnauthorized: false
     },
